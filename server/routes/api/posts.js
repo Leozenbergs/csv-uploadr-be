cddc9e396037ee164c0e828e578cc297edcad571
leozenbergs <leoabreu14@gmail.com>
2019-07-27 16:02:24 -0300

    add table contents

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -7,21 +7,44 @@ const upload = multer({
 });
 
 const router = express.Router();
+async function connection(){
+    const client = mongodb.MongoClient.connect('mongodb+srv://admin:admin@prevision-test-hqcvc.mongodb.net/test?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        server: {    
+            auto_reconnect: true,    
+            socketOptions: {
+        
+                keepAlive: 1,    
+                connectTimeoutMS: 10000,    
+                socketTimeoutMS: 10000,    
+            }    
+            },    
+            replset: {    
+            auto_reconnect: true,    
+            socketOptions: {    
+                keepAlive: 1,    
+                connectTimeoutMS: 10000,    
+                socketTimeoutMS: 10000,    
+            }    
+        }
+    });
+
+    return client;
+}
 
 
 // Get posts 
 
 router.get('/', async (req, res) => {
     const posts = await loadPosts();
-    res.send(await posts.find({}).toArray());
-    mongodb.close();
+    res.send(await posts.toArray());
 });
 
 // Add post 
 router.post('/', upload.single('file'), async (req, res) => {
     res.json({  file: req.file  });
 
-    const posts = await loadPosts();
+    const conn = await connection();
 
     let csvFilePath = `${req.file.path}`;
     // csvtojson
@@ -29,12 +52,10 @@ router.post('/', upload.single('file'), async (req, res) => {
     csv()
     .fromFile(csvFilePath)
     .then((jsonObj)=>{
-        posts.insertMany(jsonObj);
+        conn.db('Prevision_test').collection('events').insertMany(jsonObj);
         res.status(201).send();
-        
+        conn.db.close();
     });
-    mongodb.close();
-    
 });
 
 // Delete post
@@ -45,28 +66,26 @@ router.post('/', upload.single('file'), async (req, res) => {
 // })
 
 async function loadPosts(){
-    const client = await mongodb.MongoClient.connect('mongodb+srv://admin:admin@prevision-test-hqcvc.mongodb.net/test?retryWrites=true&w=majority', {
-        useNewUrlParser: true,
-        server: {    
-            auto_reconnect: true,    
-            socketOptions: {
-      
-              keepAlive: 1,    
-              connectTimeoutMS: 60000,    
-              socketTimeoutMS: 60000,    
-            }    
-          },    
-          replset: {    
-            auto_reconnect: true,    
-            socketOptions: {    
-              keepAlive: 1,    
-              connectTimeoutMS: 60000,    
-             socketTimeoutMS: 60000,    
-            }    
-         }
-    });
+    const conn = await connection();
 
-    return client.db('Prevision_test').collection('events');
+    return conn.db('Prevision_test').collection('events').aggregate([
+        {
+            $lookup:{
+                from: "companies",
+                localField : "company_id",
+                foreignField : "company_id",
+                as : "company_info"
+            }
+        },   
+        {
+            $lookup:{
+                from: "users", 
+                localField: "user_id", 
+                foreignField: "user_id",
+                as: "user"
+            }
+        }
+    ]);
     
 }
 
